1a2f6f717cc8553b429aae764611a1f945f08cf8
sclin0323@gmail.com <sclin0323@gmail.com>
2016-07-07 01:50:43 +0800

    add angular2 material

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import {HTTP_PROVIDERS} from '@angular/http';
 import { TestMaterialng2AppComponent, environment } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(TestMaterialng2AppComponent);
+bootstrap(TestMaterialng2AppComponent, [HTTP_PROVIDERS]);
 
--- src/system-config.ts
+++ src/system-config.ts
@@ -3,10 +3,62 @@
  **********************************************************************************************/
 /** Map relative paths to URLs. */
 const map: any = {
+  '@angular2-material': 'vendor/@angular2-material'
 };
 
 /** User packages configuration. */
 const packages: any = {
+  '@angular2-material/core': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'core.js'
+  },
+  '@angular2-material/checkbox': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'checkbox.js'
+  },
+  '@angular2-material/sidenav': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'sidenav.js'
+  },
+  '@angular2-material/toolbar': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'toolbar.js'
+  },
+  '@angular2-material/button': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'button.js'
+  },
+  '@angular2-material/list': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'list.js'
+  },
+  '@angular2-material/card': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'card.js'
+  },
+  '@angular2-material/input': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'input.js'
+  },
+  '@angular2-material/radio': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'radio.js'
+  },
+  '@angular2-material/icon': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'icon.js'
+  }
+
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -16,6 +68,7 @@ const packages: any = {
 const barrels: string[] = [
   // Angular specific barrels.
   '@angular/core',
+  '@angular/forms',
   '@angular/common',
   '@angular/compiler',
   '@angular/http',
